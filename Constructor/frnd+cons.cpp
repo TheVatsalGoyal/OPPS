372afcd3d4af3bf2fac51e40f79beb1b32742d62
TheVatsalGoyal <vg978653@gmail.com>
2026-09-23 11:08:26 +0530

    .

---
+++ Constructor/frnd+cons.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Test;
+class Example{
+    int a;
+    public:
+    Example(int);
+    void displayA();
+    friend void sum(Example,Test);
+};
+
+class Test{
+    int b;
+    public:
+    void displayB();
+    Test(int);
+    friend void sum(Example,Test);
+};
+
+Example::Example(int x){
+    a=x;
+}
+
+void Example::displayA(){
+    cout<<a<<"\v";
+}
+
+Test::Test(int y){
+    b=y;
+}
+
+void Test::displayB(){
+    cout<<b<<"\v";
+}
+
+void sum(Example E,Test T){
+    cout<<E.a+T.b<<" ";
+}
+
+
+int main(){
+    Example E1(10);
+    E1.displayA();
+    Test T1=Test(20);
+    T1.displayB();
+    sum(E1,T1);
+}
